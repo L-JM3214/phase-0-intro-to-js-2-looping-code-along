75d6b2590a0517985f424301af8e16cc6faabe06
Lactor <lactorjm4@gmail.com>
2023-08-01 18:10:11 +0300

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(spy, giftType) {
+    const thankYouMessages = [];
+    for (let i = 0; i < spy.length; i++) {
+      thankYouMessages.push(`Thank you, ${spy[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
